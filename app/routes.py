8a356a748400efbaa0325beeee4f284f9c249baf
Muhammed Yusuf <myusuf2121@gmail.com>
2020-07-26 11:51:08 +0300

    added profile edit feature and working rn. last seen feature is functionable but not like i want Last seen will be fixed.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app, db
-from app.forms import LoginForm, RegisterationForm
+from app.forms import LoginForm, RegisterationForm, EditProfileForm
 from app.models import User
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -81,3 +84,28 @@ def user(username):
 
     return render_template('user.html', user=user, posts=posts)
     
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Changes have been saved!')
+        return redirect(url_for('edit_profile'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                            form=form
+    )
